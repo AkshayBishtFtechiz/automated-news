3003be1b062f16b20231a78743c6a81d9ee66573
Shubham <spal5776@gmail.com>
2024-01-09 23:12:40 +0530

    formatted the email sent code

--- server/utils/emailSent.js
+++ server/utils/emailSent.js
@@ -1,65 +1,65 @@
 const nodemailer = require("nodemailer");
 
 const emailSent = async (req, res, getAllNews, firmData, newsSchema) => {
-
   if (getAllNews.length === 0) {
-        firmData.forEach(async function (data, index) {
-          const newResponse = data.payload;
-          const newNews = new newsSchema({ firm: data.firm, payload: newResponse });
-          newNews.save();
-        });
-        res.json(firmData);
-      }
+    firmData.forEach(async function (data, index) {
+      const newResponse = data.payload;
+      const newNews = new newsSchema({ firm: data.firm, payload: newResponse });
+      newNews.save();
+    });
+    res.json(firmData);
+  } 
   else if (getAllNews.length !== firmData.length) {
-        firmData.forEach(async function (data, index) {
-          if (
-            getAllNews.length > 0 &&
-            getAllNews[index]?.urlToRelease !==
-              data.payload.urlToRelease &&
-            getAllNews[index] === undefined
-          ) {
-            console.log("dataFirm:",data)
-            firmData.push({ firm: data.firm, payload: data.payload });
-            const newNews = new newsSchema({ firm: data.firm, payload: data.payload });
-            newNews.save();
-  
-            // Sending Email
-  
-            const transporter = nodemailer.createTransport({
-              service: "gmail",
-              auth: {
-                user: "automatednews21@gmail.com",
-                pass: "ovig lcvq nfdn whsj",
-              },
-              secure: false,
-              port: 25,
-              tls: {
-                rejectUnauthorized: false,
-              },
-            });
-  
-            // Define the email options
-            const mailOptions = {
-              from: "automatednews21@gmail.com",
-              to: "shubham.pal@ftechiz.com",
-              subject: `Alert: First Press Release for ${data?.payload?.tickerSymbol}`,
-              html: `<p><span style='font-weight:bold;'>${data.firm}</span> issued a press release for <span style='font-weight:bold;'>${data?.payload?.tickerSymbol}</span>. This is the first press release observed for <span style='font-weight:bold;'>${data?.payload?.tickerSymbol}</span> in the past 60 days. View the release here: ${data?.payload?.urlToRelease}.</p>`,
-            };
-  
-            // Send the email
-            transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                return console.error("Error:", error.message);
-              }
-              console.log("Email sent:", info.response);
-            });
-          }
+    firmData.forEach(async function (data, index) {
+      if (
+        getAllNews.length > 0 &&
+        getAllNews[index]?.urlToRelease !== data.payload.urlToRelease &&
+        getAllNews[index] === undefined
+      ) {
+        firmData.push({ firm: data.firm, payload: data.payload });
+        const newNews = new newsSchema({
+          firm: data.firm,
+          payload: data.payload,
         });
-  
-        res.json(firmData);
-      } else {
-        res.send(firmData);
+        newNews.save();
+
+        // Sending Email
+
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "automatednews21@gmail.com",
+            pass: "ovig lcvq nfdn whsj",
+          },
+          secure: false,
+          port: 25,
+          tls: {
+            rejectUnauthorized: false,
+          },
+        });
+
+        // Define the email options
+        const mailOptions = {
+          from: "automatednews21@gmail.com",
+          to: "shubham.pal@ftechiz.com",
+          subject: `Alert: First Press Release for ${data?.payload?.tickerSymbol}`,
+          html: `<p><span style='font-weight:bold;'>${data.firm}</span> issued a press release for <span style='font-weight:bold;'>${data?.payload?.tickerSymbol}</span>. This is the first press release observed for <span style='font-weight:bold;'>${data?.payload?.tickerSymbol}</span> in the past 60 days. View the release here: ${data?.payload?.urlToRelease}.</p>`,
+        };
+
+        // Send the email
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            return console.error("Error:", error.message);
+          }
+          console.log("Email sent:", info.response);
+        });
       }
-}
+    });
+
+    res.json(firmData);
+  } else {
+    res.send(firmData);
+  }
+};
 
 module.exports = emailSent;
